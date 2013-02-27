27bd69da33edb4b4d7b853487e28a2b77c87ad68
jamietech <jamie@jamiete.ch>
2013-02-27 17:45:28 +1000

    Update player list

--- src/to/joe/j2mc/info/command/PlayerListCommand.java
+++ src/to/joe/j2mc/info/command/PlayerListCommand.java
@@ -1,5 +1,8 @@
 package to.joe.j2mc.info.command;
 
+import java.util.HashSet;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,79 +19,75 @@ public class PlayerListCommand extends MasterCommand<J2MC_Info> {
 
     @Override
     public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
-        if (!sender.hasPermission("j2mc.core.admin")) {
-            int total = 0;
-            for(Player derp : plugin.getServer().getOnlinePlayers()){
-                if(!J2MC_Manager.getVisibility().isVanished(derp)){
-                    total++;
-                }
-            }
-            sender.sendMessage(ChatColor.AQUA + "Players (" + total + "/" + plugin.getServer().getMaxPlayers() + "):");
-            if (total == 0) {
-                sender.sendMessage(ChatColor.RED + "No one is online :(");
-                return;
-            }
-            StringBuilder builder = new StringBuilder();
-            for (Player pl : plugin.getServer().getOnlinePlayers()) {
-                if (!J2MC_Manager.getVisibility().isVanished(pl)) {
-                    String toAdd;
-                    toAdd = pl.getDisplayName();
-                    if(J2MC_Manager.getPermissions().hasFlag(pl.getName(), 'd')){
-                        toAdd = ChatColor.GOLD + pl.getName();
+        final List<Player> online = J2MC_Manager.getVisibility().getOnlinePlayers(player);
+        final boolean detail = isPlayer && player.hasPermission("j2mc.core.admin");
+        final StringBuilder sb = new StringBuilder();
+
+        sender.sendMessage(ChatColor.AQUA + "Players (" + online.size() + "/" + this.plugin.getServer().getMaxPlayers() + "):");
+
+        if (online.isEmpty()) {
+            sender.sendMessage(ChatColor.RED + "No one is online :(");
+            return;
+        }
+
+        for (final Player user : online) {
+            if (detail) {
+                String name = user.getName();
+                final HashSet<Character> flags = J2MC_Manager.getPermissions().getFlags(name);
+
+                if (flags.isEmpty()) {
+                    sb.append(ChatColor.GREEN + name + ", ");
+                } else {
+                    for (final Character flag : flags) {
+                        if (J2MC_Manager.getVisibility().isVanished(user)) { // Vanished
+                            name = ChatColor.AQUA + name;
+                        } else if (flag == 's') { // Senior
+                            name = ChatColor.DARK_RED + name;
+                        } else if (flag == 'a') { // Admin
+                            name = ChatColor.RED + name;
+                        } else if (flag == 'd') { // Donator
+                            name = ChatColor.GOLD + name;
+                        } else if (flag == 't') { // Trusted
+                            name = ChatColor.DARK_GREEN + name;
+                        } else { // Normal
+                            name = ChatColor.GREEN + name;
+                        }
+
+                        // Trusted
+                        if ((flag == 't') && !name.contains(ChatColor.DARK_GREEN.toString())) {
+                            name += ChatColor.DARK_GREEN + "[T]";
+                        }
+
+                        // Muted
+                        if (flag == 'M') {
+                            name += ChatColor.YELLOW + "[M]";
+                        }
+
+                        // NSA
+                        if (flag == 'N') {
+                            name += ChatColor.RED + "\u00ab\u00bb";
+                        }
+
+                        // Teleport Banned
+                        if (flag == 'T') {
+                            name += ChatColor.STRIKETHROUGH + "[TP]" + ChatColor.RESET;
+                        }
+
+                        // Harassed
+                        if (flag == 'H') {
+                            name += ChatColor.AQUA + "[H]";
+                        }
                     }
-                    builder.append(toAdd + ChatColor.WHITE + ", ");
-                    if (builder.length() > 75) {
-                        builder.setLength(builder.length() - (toAdd + ChatColor.WHITE + ", ").length());
-                        sender.sendMessage(builder.toString());
-                        builder = new StringBuilder();
-                        builder.append(toAdd + ChatColor.WHITE + ", ");
-                    }
+
+                    name += ChatColor.WHITE + ", ";
+                    sb.append(name);
                 }
+            } else {
+                sb.append(user.getDisplayName() + ", ");
             }
-            builder.setLength(builder.length() - 2);
-            sender.sendMessage(builder.toString());
-        } else {
-            sender.sendMessage(ChatColor.AQUA + "Players (" + plugin.getServer().getOnlinePlayers().length + "/" + plugin.getServer().getMaxPlayers() + "):");
-            if (plugin.getServer().getOnlinePlayers().length == 0) {
-                sender.sendMessage(ChatColor.RED + "No one is online :(");
-                return;
-            }
-            StringBuilder builder = new StringBuilder();
-            for (Player pl : plugin.getServer().getOnlinePlayers()) {
-                String toAdd = "";
-                toAdd = ChatColor.GREEN + pl.getName();
-                if(J2MC_Manager.getPermissions().hasFlag(pl.getName(), 't')){
-                    toAdd = ChatColor.DARK_GREEN + pl.getName();
-                }
-                if(J2MC_Manager.getPermissions().hasFlag(pl.getName(), 'd')){
-                    toAdd = ChatColor.GOLD + pl.getName();
-                }
-                if(pl.hasPermission("j2mc.chat.mute")){
-                    toAdd = ChatColor.YELLOW + pl.getName();
-                }
-                if(pl.hasPermission("j2mc.core.admin")){
-                    toAdd = ChatColor.RED + pl.getName();
-                }
-                if(J2MC_Manager.getVisibility().isVanished(pl)){
-                    toAdd = ChatColor.AQUA + pl.getName();
-                }
-                if(J2MC_Manager.getPermissions().hasFlag(pl.getName(), 'N')){
-                    toAdd += ChatColor.DARK_AQUA + "\u00ab\u00bb";
-                }
-                if(J2MC_Manager.getPermissions().hasFlag(pl.getName(), 'k')){
-                    toAdd += ChatColor.RED + "&";
-                }
-                builder.append(toAdd + ChatColor.WHITE + ", ");
-                if (builder.length() > 75) {
-                    builder.setLength(builder.length() - (toAdd + ChatColor.WHITE + ", ").length());
-                    sender.sendMessage(builder.toString());
-                    builder = new StringBuilder();
-                    builder.append(toAdd + ChatColor.WHITE + ", ");
-                }
-            }
-            builder.setLength(builder.length() - 2);
-            sender.sendMessage(builder.toString());
+
+            sb.setLength(sb.length() - 2);
+            sender.sendMessage(sb.toString());
         }
     }
-
 }
